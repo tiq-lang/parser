2f332a47db39b53f42f10f4d26695f8121ba39e6
Mikhail Kaluzhnyy <teviroff@gmail.com>
2025-08-21 18:55:39 +0300

    identifier primitive (#4)

---
+++ parser-lang/src/byte_stream.rs
@@ -0,0 +1,5 @@
+//! This module contains language specific byte stream functionality.
+//!
+//! More specific information about different submodules can be found on their respective pages.
+
+pub mod extensions;
